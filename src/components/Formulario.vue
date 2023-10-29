39087b241b2cee577f4f045c050ce119c104d6d3
tony tolver <andrademaresantonio00@gmail.com>
2023-10-29 01:22:51 -0600

    leer los datos del formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,19 +1,25 @@
 <script setup>
+  import { ref } from 'vue'
+
+  const nombre = ref('maximo')
+
+  const leerNombre = (e) => {
+    nombre.value = e.target.value
+  }
 
 </script>
 
 <template>
   <div class="md:w-1/2">
     <h2 class="font-black text-3xl text-center">Seguimientos Pacientes</h2>
-
     <p class="text-lg mt-5 text-center mb-10">
       Añade Pacientes y
       <span class="text-indigo-600 font-bold">Adminístralos</span>
     </p>
-
     <form 
       class="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
-    >
+    > 
+      {{ nombre }}
       <div class="mb-5">
         <label
             for="mascota"
@@ -25,6 +31,8 @@
             type="text"
             placeholder="Nombre de la mascota"
             class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            :value="nombre"
+            @input="leerNombre"
             >
       </div>
       <div class="mb-5">
