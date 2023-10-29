966e3ca33ec46673e47175bdf758108c2c69d33c
tony tolver <andrademaresantonio00@gmail.com>
2023-10-29 13:14:41 -0600

    ADD componente para mostrar alertas

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -34,6 +34,12 @@
       Añade Pacientes y
       <span class="text-indigo-600 font-bold">Adminístralos</span>
     </p>
+
+    <Alerta    
+        v-if="alerta.mensaje"
+        :alerta="alerta"
+    />
+      
     <form 
         class="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
         @submit.prevent="validar"
