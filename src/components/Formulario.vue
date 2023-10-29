5bb4e6426ae2b0a3466f69347dac0b2328121198
tony tolver <andrademaresantonio00@gmail.com>
2023-10-29 01:25:39 -0600

    ADD lectura de datos ingresados con v-model

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,11 +1,7 @@
 <script setup>
   import { ref } from 'vue'
 
-  const nombre = ref('maximo')
-
-  const leerNombre = (e) => {
-    nombre.value = e.target.value
-  }
+  const nombre = ref('')
 
 </script>
 
@@ -16,6 +12,7 @@
       Añade Pacientes y
       <span class="text-indigo-600 font-bold">Adminístralos</span>
     </p>
+    {{ nombre  }}
     <form 
       class="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
     > 
@@ -31,8 +28,7 @@
             type="text"
             placeholder="Nombre de la mascota"
             class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-            :value="nombre"
-            @input="leerNombre"
+            v-model="nombre"
             >
       </div>
       <div class="mb-5">
