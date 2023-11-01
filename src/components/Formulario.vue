7e76e696b719e7b26132a2e96fc8c54d5a37ea02
tony tolver <andrademaresantonio00@gmail.com>
2023-10-31 19:00:16 -0600

    Validar los datos ingresados desde el componente hijo

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -8,16 +8,33 @@
     titulo: ''
   })
 
-  const paciente = reactive({
-    nombre: '',
-    propietario: '',
-    email: '',
-    alta: '',
-    sintomas: ''
+  defineEmits(['update:nombre', 'update:propietario', 'update:email', 'update:alta', 'update:sintomas'])
+
+  const props = defineProps({
+    nombre: {
+      type: String,
+      required: true
+    },
+    propietario: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    alta: {
+      type: String,
+      required: true
+    },
+    sintomas: {
+      type: String,
+      required: true
+    }
   })
 
   const validar = (e) =>{
-    if (Object.values(paciente).includes('')) {
+    if (Object.values(props).includes('')) {
       alerta.mensaje = 'Todos los campos son obligatorios';
       alerta.tipo = 'error';
       return;
@@ -57,7 +74,8 @@
             type="text"
             placeholder="Nombre de la mascota"
             class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-            v-model="paciente.nombre"
+            :value="nombre"
+            @input="$emit('update:nombre', $event.target.value)"
             >
       </div>
       <div class="mb-5">
@@ -71,7 +89,8 @@
             type="text"
             placeholder="Nombre del propietario"
             class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-            v-model="paciente.propietario"
+            :value="propietario"
+            @input="$emit('update:propietario', $event.target.value)"
             >
       </div>
       <div class="mb-5">
@@ -85,7 +104,8 @@
             type="email"
             placeholder="Email del propietario"
             class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-            v-model="paciente.email"
+            :value="email"
+            @input="$emit('update:email', $event.target.value)"
             >
       </div>
       <div class="mb-5">
@@ -98,7 +118,8 @@
             id="alta"
             type="date"
             class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-            v-model="paciente.alta"
+            :value="alta"
+            @input="$emit('update:alta', $event.target.value)"
             >
       </div>
       <div class="mb-5">
@@ -111,7 +132,8 @@
             id="sintomas"
             placeholder="Describe los sintomas"
             class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"
-            v-model="paciente.sintomas"
+            :value="sintomas"
+            @input="$emit('update:sintomas', $event.target.value)"
             />
       </div>
 
